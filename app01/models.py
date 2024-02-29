b396ed05c2b3f66e3bd891f1fd0d4ecdfd384b3e
DWZ <473755228@qq.com>
2024-02-29 10:56:52 +0800

    dwz

---
+++ app01/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class UserInfo(models.Model):
+    # 创建字段
+    name = models.CharField(max_length=32)
+    password = models.CharField(max_length=64)
+    age = models.IntegerField()
