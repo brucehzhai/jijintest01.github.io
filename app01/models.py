913da1fac08ccb93808c43a7ce147456b7dc5a69
bruce <hyu48672@gmail.com>
2024-03-02 16:17:37 +0800

    18_2

--- app01/models.py
+++ app01/models.py
@@ -3,13 +3,12 @@ from django.db import models
 # Create your models here.
 class UserInfo(models.Model):
     # 创建字段
-    name = models.CharField(verbose_name="用户名",max_length=32)
-    password = models.CharField(verbose_name="密码",max_length=64)
-    age = models.IntegerField(verbose_name="年龄")
-    gender_choices = (
-        (1, "男"),
-        (2, "女"),
-    )
-    gender = models.SmallIntegerField(verbose_name="员工性别", choices=gender_choices)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    sex = models.CharField(max_length=255, blank=True, null=True)
+    age = models.IntegerField(blank=True, null=True)
+    pwd = models.CharField(max_length=255, blank=True, null=True)
+    email = models.CharField(max_length=255, blank=True, null=True)
+
+
 
 
--- app01/views.py
+++ app01/views.py
@@ -4,9 +4,24 @@ from django.shortcuts import render,HttpResponse
 def index(request):
     return HttpResponse('HELLO WORLD')
 
+
+def register(request):
+    if request.method == "GET":
+        return render(request, "Register.html")
+    username = request.POST.get("name")
+    if models.Userinfo.objects.filter(name=username):
+        return render(request, "Login.html", {"error_msg": "该账号已被注册"})
+    password = request.POST.get("password")
+    if not username or not password:
+        return render(request, "Register.html", {"error_msg": "用户名或密码不能为空"})
+    email = request.POST.get("email")
+    models.Userinfo.objects.create(name=username, pwd=password)
+    print("注册成功")
+    return render(request, "Login.html", {"error_msg": "您已注册成功，请登录！~"})
+
 def login(request):
     if request.method == "GET":
-        return render(request, "tea_login.html")
+        return render(request, "Login.html")
 
     # 如果是POST请求，获取用户提交的数据
     print(request.POST)
@@ -21,32 +36,6 @@ def login(request):
         print(request.session["info"])
         request.session.set_expiry(60 * 60 * 24 * 7)
 
-        return redirect("/tea_index/")
+        return redirect("index.html")
     else:
-        return render(request, "tea_login.html", {"error_msg": "用户名或密码错误"})
-
-
-def register(request):
-    if request.method == "GET":
-        return render(request, "Register.html")
-    username = request.POST.get("name")
-    if models.Userinfo.objects.filter(name=username):
-        return render(request, "tea_login.html", {"error_msg": "该账号已被注册"})
-    sex = request.POST.get('sex')
-    age = request.POST.get('age')
-    password = request.POST.get("pwd")
-    activation_code = string.ascii_uppercase + '0123456789'
-    print(activation_code)
-    ivt = ''
-    for i in range(10):
-        ivt += random.choice(activation_code)
-    try:
-        code = request.POST.get('code')
-        score_1 = models.Userinfo.objects.get(ivt=code).score
-        models.Userinfo.objects.filter(ivt=code).update(score=score_1 + 100)
-    except Exception as e:
-        print(e)
-    finally:
-        models.Userinfo.objects.create(name=username, pwd=password, sex=sex, age=age, ivt=ivt, score=0)
-        print("注册成功")
-    return render(request, "tea_login.html", {"error_msg": "您已注册成功，请登录！~"})
+        return render(request, "Login.html", {"error_msg": "用户名或密码错误"})
