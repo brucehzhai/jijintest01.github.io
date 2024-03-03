3b536372edea5e0b80d86d0839b005b3ecaa83d6
bruce <hyu48672@gmail.com>
2024-03-03 10:32:02 +0800

    18_2

--- app01/views.py
+++ app01/views.py
@@ -1,49 +1,52 @@
-from django.shortcuts import render,HttpResponse
-from django.db.models import Q
-from app01 import models
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password, check_password
-from django.http import HttpResponseServerError
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse, HttpResponseServerError
+from app01 import models
+
 
 from app01.models import userinfo
 
 # Create your views here.
 def index(request):
-    return HttpResponse('HELLO WORLD')
+    return render(request, 'index.html')
 
 
 def register(request):
-        if request.method == 'POST':
-            # 如果是POST请求，则执行以下代码
-            name = request.POST.get('name')
-            password = request.POST.get('password')
-            email = request.POST.get('email')
+    if request.method == 'POST':
+        # 如果是POST请求，则执行以下代码
+        name = request.POST.get('name')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
 
-            # 在这里进行用户输入的验证逻辑
+        # 在这里进行用户输入的验证逻辑
 
-            userinfo.objects.create(name=name, email=email, password=password)
-            return render(request, 'Login.html')
-        else:
-            # 如果是GET请求，则执行以下代码
-            return render(request, 'register.html')
+        userinfo.objects.create(name=name, email=email, password=password)
+        return redirect('/login/')
+
+    else:
+        # 如果是GET请求，则执行以下代码
+        return render(request, 'register.html')
+
+
+
+
+
+# Create your views here.
 
 def login(request):
     if request.method == "GET":
         return render(request, "Login.html")
 
     # 如果是POST请求，获取用户提交的数据
-    print(request.POST)
     name = request.POST.get('name')
-    pwd = request.POST.get('pwd')
-    if models.Userinfo.objects.filter(
-            Q(name=name) & Q(pwd=pwd)
+    password = request.POST.get('password')
+    if models.userinfo.objects.filter(
+            Q(name=name) & Q(password=password)
     ):
-        # 网站生成随机字符串; 写到用户浏览器的cookie中；在写入到session中；
-        request.session["info"] = {'name': name}
-        # session可以保存7天
-        print(request.session["info"])
-        request.session.set_expiry(60 * 60 * 24 * 7)
+
+        return redirect('/index/')
 
 
-    else:
-        return render(request, "Login.html", {"error_msg": "用户名或密码错误"})
+
