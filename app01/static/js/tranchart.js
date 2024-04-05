144b48d6f2d7b6b52e81f774a000c7fafdd26d66
CalmeSad <2645463200@qq.com>
2024-04-05 11:43:04 +0800

    qqqq

--- app01/static/js/tranchart.js
+++ app01/static/js/tranchart.js
@@ -1,5 +1,5 @@
 (function () {
-    var chartDom = document.getElementById('tcharts');
+    var chartDom = document.getElementById('productbar');
 var myChart = echarts.init(chartDom);
 var option;
 
@@ -99,106 +99,7 @@ option && myChart.setOption(option);
 
 })();
 
-(function () {
 
-var chartDom = document.getElementById('productbar');
-var myChart = echarts.init(chartDom);
-var option;
-
-option = {
-  tooltip: {
-    trigger: 'axis',
-    axisPointer: {
-      type: 'cross',
-      crossStyle: {
-        color: '#999'
-      }
-    }
-  },
-  toolbox: {
-    feature: {
-      dataView: { show: true, readOnly: false },
-      magicType: { show: true, type: ['line', 'bar'] },
-      restore: { show: true },
-      saveAsImage: { show: true }
-    }
-  },
-  legend: {
-    data: ['产值', '产量', '增速']
-  },
-  xAxis: [
-    {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-      axisPointer: {
-        type: 'shadow'
-      }
-    }
-  ],
-  yAxis: [
-    {
-      type: 'value',
-      name: '产量',
-      min: 0,
-      max: 250,
-      interval: 50,
-      axisLabel: {
-        formatter: '{value} ml'
-      }
-    },
-    {
-      type: 'value',
-      name: '增速',
-      min: 0,
-      max: 25,
-      interval: 5,
-      axisLabel: {
-        formatter: '{value} %'
-      }
-    }
-  ],
-  series: [
-    {
-      name: '产值',
-      type: 'bar',
-      tooltip: {
-        valueFormatter: function (value) {
-          return value + ' ml';
-        }
-      },
-      data: [
-        2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3
-      ]
-    },
-    {
-      name: '产量',
-      type: 'bar',
-      tooltip: {
-        valueFormatter: function (value) {
-          return value + ' ml';
-        }
-      },
-      data: [
-        2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3
-      ]
-    },
-    {
-      name: '增速',
-      type: 'line',
-      yAxisIndex: 1,
-      tooltip: {
-        valueFormatter: function (value) {
-          return value + ' °C';
-        }
-      },
-      data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
-    }
-  ]
-};
-
-option && myChart.setOption(option);
-
-})();
 
 (function () {
 
